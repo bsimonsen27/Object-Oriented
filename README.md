e1bca93e6d760863ee97326789c57657d8d85b6b
Brendon Simonsen <brendonsimonsen@mail.weber.edu>
2021-04-12 08:48:17 -0600

    added attributes

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Object Oriented Programming
 
 - programming paradigm
+
+# Class
+
+- Blueprint of an object
+- Not the object
+
+# Object
+
+- Attribute (Something that describes the object)
+    - Car Object (Attribute --> color --> car1 --> color('Blue'))
--- students.py
+++ students.py
@@ -1,18 +1,13 @@
 class student:
-    pass
+    def __init__(self, first, last, status):
+        self.first = first #these are instance variables
+        self.last = last
+        self.status = status
+        self.email = first + last + '@mail.weber.edu'
 
-W01234 = student()
-W01235 = student()
+W01234 = student('Scott', 'Hazdik', 'Pass')
+W01235 = student('Waldo', 'Wildcat', 'Pass')
 
-W01234.first = "Scott"
-W01234.last = "Hadzik"
-W01234.email = "scotthadzik@weber.edu"
-W01234.status = "Pass"
 
-W01235.first = "Waldo"
-W01235.last = "Wildcat"
-W01235.email = "waldowildcat@weber.edu"
-W01235.status = "Pass"
-
-print(W01234.email)
-print(W01235.email)
+print(W01234.first, W01234.last, W01234.email, W01234.status)
+print(W01235.first, W01235.last, W01235.email, W01235.status)
